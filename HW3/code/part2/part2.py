baf71c6a091088b0dceb91c6b967659c14a1e071
tyunist <tynguyen@unist.ac.kr>
2017-11-14 21:05:04 -0500

    Fix printing

--- HW3/code/part2/part2.py
+++ HW3/code/part2/part2.py
@@ -133,7 +133,7 @@ lr_array = []
 # Find anchors (x_a, y_a, w_a) N x 3 x 36 
 mask_indices = utils.map_mask_2_img_coordinates()  # (2 x 36)
 # TODO: set this value to a proper value
-w_a = 40 # I think it should be 40 but TA said it is 32 
+w_a = 32# I think it should be 40 but TA said it is 32 
 w_a_array = w_a*np.ones(36)
 mask_indices = np.vstack([mask_indices, w_a_array])
  
@@ -159,8 +159,8 @@ def train(epoch, max_iter=None, lr=0, visual=False, is_train=True, best_acc=None
   train_class_loss = 0 
   train_reg_loss = 0 
 
-  #correct = 0 
-  #total = 0
+  correct = 0 
+  total = 0
   num_iter = 0  
   dataloader_queue = trainloader if is_train else testloader
 
@@ -209,13 +209,6 @@ def train(epoch, max_iter=None, lr=0, visual=False, is_train=True, best_acc=None
     
     
     # Get accuracy 
-    #max_outputs, _ = torch.max(isobject_outputs, 1, keepdim=True) 
-    #center_predict = isobject_outputs.eq(max_outputs)
-    #total += batch_size
-
-    #correct += torch.masked_select(masks.view(, center_predict).eq(1).float().cpu().sum().data.numpy()[0]  
-    
-    # Get accuracy 
     pos_thresh = 0.5 
     use_sigmoid = True 
     if use_sigmoid:
@@ -224,8 +217,8 @@ def train(epoch, max_iter=None, lr=0, visual=False, is_train=True, best_acc=None
       sigmoided_isobject_outputs = isobject_outputs
     pos_pred = (sigmoided_isobject_outputs >= pos_thresh).float()
     neg_pred = (sigmoided_isobject_outputs < pos_thresh).float() 
-    correct  = (pos_pred * one_filter).sum().cpu().data.numpy()[0] + (neg_pred * zero_filter).sum().cpu().data.numpy()[0]
-    total    =  one_filter.sum().cpu().data.numpy()[0] + zero_filter.sum().cpu().data.numpy()[0]
+    correct  += (pos_pred * one_filter).sum().cpu().data.numpy()[0] + (neg_pred * zero_filter).sum().cpu().data.numpy()[0]
+    total    +=  one_filter.sum().cpu().data.numpy()[0] + zero_filter.sum().cpu().data.numpy()[0]
     #print('Correct:', correct, 'total:', total)  
     class_loss  = isobject_criterion(isobject_outputs.view(-1), masks.view(-1)) # evarage over minibatch 
 
@@ -245,11 +238,9 @@ def train(epoch, max_iter=None, lr=0, visual=False, is_train=True, best_acc=None
     train_class_loss += class_loss.data[0] 
     train_reg_loss += reg_loss.data[0] 
     train_loss += total_loss.data[0]
-    #epoch_time = progress_bar(batch_idx, len(trainloader), 'Is train: %d  | Total Loss: %.3f | Class Loss: %.4f |Reg Loss: %.4f |Acc: %.3f%% | (%d/%d) | lr: %.6f'
-        #  % (int(is_train), train_loss/(batch_idx+1), train_class_loss/(batch_idx+1), train_reg_loss/(batch_idx+1), 100.*correct/total, correct, total,  lr))
-    
     epoch_time = progress_bar(batch_idx, len(trainloader), 'Is train: %d  | Total Loss: %.3f | Class Loss: %.4f |Reg Loss: %.4f |Acc: %.3f%% | (%d/%d) | lr: %.6f'
-          % (int(is_train), total_loss.data[0], class_loss.data[0], reg_loss.data[0], 100.*correct/total, correct, total,  lr))
+          % (int(is_train), train_loss/(batch_idx+1), train_class_loss/(batch_idx+1), train_reg_loss/(batch_idx+1), 100.*correct/total, correct, total,  lr))
+    
     
     num_iter+= 1 
     
@@ -288,15 +279,15 @@ def run():
   best_acc = 0 
 
   for epoch in range(start_epoch, start_epoch+args.max_epoches):
-    lr, epoch_time, train_acc, train_loss, train_class_loss, train_reg_loss, _ = train(epoch, 100, lr, args.visual)
-    training_time += epoch_time 
     if epoch == args.max_epoches-1:
       max_batches = 100
     else: 
-      max_batches = 2 
+      max_batches = 20 
+    _, _, test_acc, test_loss, test_class_loss, test_reg_loss, best_acc = train(epoch, max_batches, is_train=False, best_acc=best_acc)
+    lr, epoch_time, train_acc, train_loss, train_class_loss, train_reg_loss, _ = train(epoch, 100, lr, args.visual)
+    training_time += epoch_time 
     
 
-    _, _, test_acc, test_loss, test_class_loss, test_reg_loss, best_acc = train(epoch, max_batches, is_train=False, best_acc=best_acc)
     
     train_acc_array.append([epoch, training_time, train_acc, train_loss, train_class_loss, train_reg_loss])
     test_acc_array.append([epoch, training_time, test_acc, test_loss, test_class_loss, test_reg_loss]) 
