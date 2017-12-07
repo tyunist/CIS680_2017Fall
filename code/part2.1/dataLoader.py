a6726c3e1323d502ce55ed1b6e4828c4c81f4246
tyunist <tynguyen@unist.ac.kr>
2017-12-07 16:58:46 -0500

    finish 2.1

---
+++ code/part2.1/dataLoader.py
@@ -0,0 +1,47 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.image as mpimg
+import pdb
+
+home_dir = '/home/tynguyen/cis680/HW4' 
+
+
+'''
+  dataloader
+'''
+def dataProcess(readTest=False):
+  dict_cur = {'img': [], 'order': []}
+  
+  # obtain data path (test or train)
+  if readTest:
+    print '>>>>>>>>>>> load testing data .................'
+    path = home_dir + '/data/cufs/devkit/test.txt'
+  else:
+    print '>>>>>>>>>>> load training data .................'
+    path = home_dir + '/data/cufs/devkit/train.txt'
+  
+  with open(path) as f:
+    for line in f:
+      file_info = line.split()
+      file_name = file_info[0]
+
+      image_path = home_dir + '/data/cufs/imgs/' + file_name
+      img = mpimg.imread(image_path)
+
+      # expand dimension to make it as shape [img_height, img_width, 1]
+      img = np.expand_dims(img, axis=2)
+
+      # image data
+      dict_cur['img'].append(img)
+
+      # img label
+      dict_cur['order'].append(int(file_info[1]))
+
+  
+  dict_cur['img'] = np.array(dict_cur['img'])
+  dict_cur['order'] = np.array(dict_cur['order'])
+
+  return dict_cur
